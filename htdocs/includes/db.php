e11b02dce2d5d47c593260fab97e3339cfa2b8cc
sudharsanspc <sudharsanpooranachandiran@gmail.com>
2026-04-19 19:11:50 +0530

    Update db.php

--- htdocs/includes/db.php
+++ htdocs/includes/db.php
@@ -1,12 +1,12 @@
 <?php
-$host = "sql313.infinityfree.com";   // un screenshot la irundhu
-$username = "if0_41696127";          // DB username
-$password = "ZP14esrSg9Ypo";      // panel la irukum (IMPORTANT)
+$host = "sql313.infinityfree.com";   
+$username = "db_username";         
+$password = "db_password";      
 $database = "ifo_41696127_bus_tracking";
 
 $conn = new mysqli($host, $username, $password, $database);
 
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
